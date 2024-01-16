98aa1fa01ccf6f221524b5e79f97792184676367
Cesaire Viegas <cviegas@bess-f1r2s2.clusters.42paris.fr>
2024-01-16 15:10:49 +0100

    squid

--- srcs/ops_rrr.c
+++ srcs/ops_rrr.c
@@ -6,14 +6,13 @@
 /*   By: cviegas <cviegas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/14 20:38:04 by cviegas           #+#    #+#             */
-/*   Updated: 2024/01/16 15:08:56 by cviegas          ###   ########.fr       */
+/*   Updated: 2024/01/16 15:10:37 by cviegas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
 // rrr : rra and rrb at the same time.
-// Last git test
 void	rrr(t_tuple *t)
 {
 	rrotate(&t->a);
